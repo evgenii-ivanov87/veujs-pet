c454a0034ad6a4aabf567ecb7e4185ad483e46bf
evgenii-ivanov87 <66361345+evgenii-ivanov87@users.noreply.github.com>
2022-03-24 22:59:19 +0200

    start

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+      <h1>{{title}}</h1>
+      <ButtonItem @click="increment" >Click me</ButtonItem>
+  
+  
+</template>
+
+<script>
+import ButtonItem from './components/Button.vue';
+
+export default {
+  name: 'App',
+  components: {
+    ButtonItem
+  },
+  data(){
+    return{
+      amountOfCliks:0
+    }
+  },
+  computed:{
+    title(){
+      return `Amount Of Cliks ${this.amountOfCliks}`
+    }
+  },
+  methods:{
+    increment(){
+      this.amountOfCliks+=1
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
+
